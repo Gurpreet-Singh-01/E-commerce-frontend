25907c75fcad8666b836fe4c94c04ce84405ccf9
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-05-16 15:40:16 +0530

    Tested add to cart slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from './store/cartSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const {items,totalQuantity,totalPrice} = useSelector((state) =>state.cart)
+
+  const testItem = {
+    id:'1',
+    name:"Test Prod",
+    price: 100,
+    image:'https://images.pexels.com/photos/32087013/pexels-photo-32087013/free-photo-of-contemplative-woman-leaning-on-disco-ball.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=loadhttps://images.pexels.com/photos/32087013/pexels-photo-32087013/free-photo-of-contemplative-woman-leaning-on-disco-ball.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+  }
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold text-primary">
-        API URL: {import.meta.env.VITE_API_BASE_URL}
-      </h1>
+    <div className='min-h-screen bg-secondary text-primary p-4'>
+      <h1 className='text-3xl font-bold text-center'>Cart: {totalQuantity} items, Rs{totalPrice}</h1>
+      <button
+        className='bg-primary text-secondary px-4 py-2 rounded'
+        onClick={()=>dispatch(addToCart(testItem))}
+      >Test Cart</button>
+
+      <ul>
+        {items.map((item) =>(
+          <li key={item.id}>{item.name} - {item.price} * {item.quantity}</li>
+        ))}
+      </ul>
+
     </div>
   );
 };
