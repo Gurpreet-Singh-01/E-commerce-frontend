8406ee7f3ed97ce26387017cb96333f638adef5f
Gurpreet-Singh-01 <gurpreet.singh.work.29@gmail.com>
2025-05-17 11:20:31 +0530

    Implemented axios interceptor for refreshing token

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,30 @@
 import axios from 'axios';
-import {updateUser,logout} from '../store/authSlice'
-
+import { updateUser, logout } from '../store/authSlice';
+import { refreshAccessToken } from './userService';
+import { store } from '../store/index';
 const api = axios.create({
-    baseURL:import.meta.env.VITE_API_BASE_URL,
-    headers: {'Content-Type' : 'application/json'},
-    withCredentials:true
-})
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const { user } = await refreshAccessToken();
+        store.dispatch(updateUser({ user }));
+        return api(originalRequest);
+      } catch (refreshError) {
+        store.dispatch(logout());
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
